9f30cf0a906251e5d8543882e9cdc62a4370ea6d
Kristian Mikkelsen <Kristian@kristians-air.hhk.dk>
2016-11-25 09:33:14 +0100

    teacherView viser nu kurser

--- js/teacher.js
+++ js/teacher.js
@@ -1,3 +1,33 @@
-/**
- * Created by Kristian on 23/11/2016.
- */
+$(document).ready(function () {
+    var teacherTableBody = $("#teacherTableBody");
+
+    $.ajax({
+        type: 'GET',
+        url: SDK.serverURL + "/course/" + SDK.Storage.load("userId"),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (course) {
+            var decrypted = $.parseJSON(SDK.Decrypt(course))
+            /*console.log(decrypted[0].code)*/
+
+
+            decrypted.forEach(function (decrypted) {
+
+                teacherTableBody.append(
+                    "<tr>" +
+                    "<td>" + decrypted.code + "</td>" +
+                    "<td>" + /*course.reviewAverage*/ +"</td>" +
+                    "<td>" + /*button*/ +"</td>" +
+                    "</tr>"
+                );
+
+            });
+
+        },
+        error: function () {
+            alert('Failed!');
+        }
+
+
+    })
+})
